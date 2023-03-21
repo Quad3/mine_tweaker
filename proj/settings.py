254c100adcf7ae2f1673106792794836eff617b3
Quad3 <bbulat1990@gmail.com>
2023-03-21 16:15:11 +0900

    fix env variables

--- proj/settings.py
+++ proj/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = env("DJANGO_SECRET_KEY", "strongsecret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = env("TIME_ZONE")
+TIME_ZONE = env("TIME_ZONE", "UTC")
 
 USE_I18N = True
 
