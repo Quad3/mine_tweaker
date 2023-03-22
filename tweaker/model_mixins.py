3ad84128e2a30a36c22939b5ef7fd97f76e46034
Quad3 <bbulat1990@gmail.com>
2023-03-22 13:35:38 +0900

    add social network models

---
+++ tweaker/model_mixins.py
@@ -0,0 +1,33 @@
+from django.db import models
+from datetime import datetime
+
+
+class RecordMixin(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+    owner = models.ForeignKey('accounts.CustomUser',
+                              on_delete=models.CASCADE,
+                              related_name='%(app_label)s_%(class)s_related',
+                              )
+
+    class Meta:
+        abstract = True
+    
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        utcnow = datetime.utcnow()
+        if not self.id:
+            self.created_at = utcnow
+        self.modified_at = utcnow
+
+        return super().save(force_insert, force_update, using, update_fields)
+
+
+class LikeIntermediaryMixin(models.Model):
+    user = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
--- tweaker/models.py
+++ tweaker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from .model_mixins import RecordMixin, LikeIntermediaryMixin
+
+
+class PostLike(LikeIntermediaryMixin):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+
+class CommentLike(LikeIntermediaryMixin):
+    comment = models.ForeignKey('Comment', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['likes']
+
+
+class Post(RecordMixin):
+    likes = models.ManyToManyField('accounts.CustomUser', through=PostLike)
+
+    class Meta:
+        ordering = ['created_at']
+
+
+class Comment(RecordMixin):
+    likes = models.ManyToManyField('accounts.CustomUser', through=CommentLike)
